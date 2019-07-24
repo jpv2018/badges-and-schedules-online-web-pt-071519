273c117ff3643e46f1c9d4e3e77e603a754c99c9
Jason Viola <jason.viola@mavs.uta.edu>
2019-07-24 02:38:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,13 @@
 def badge_maker(name)
-  p "Hello, my name is #{name}"
+  p "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(names)
+  greeting = [ ]
   names.each do |name|
-    return "Hello, my name is #{name}"
+    greeting.push("Hello, my name is #{name}")
   end
+  return greeting
 end
 
 def assign_rooms(names, rooms)
